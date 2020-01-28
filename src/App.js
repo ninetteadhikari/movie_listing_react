bc933f58833854185fd3b6ecaeda9b0ecd89ea46
Ninette <ninette.adhikari@gmail.com>
2020-01-28 17:15:42 +0100

    added 10 pages of movielisting

--- src/App.js
+++ src/App.js
@@ -9,26 +9,31 @@ class App extends Component {
   };
 
   componentDidMount() {
-    axios
-      .get(
-        `http://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}`
-      )
-      .then(response => {
-        this.setState({
-          movieList: response.data.results
+    for (let i = 1; i <= 10; i++) {
+      axios
+        .get(
+          `http://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&page=${i}`
+        )
+        .then(response => {
+          this.setState(state => {
+            const movieList = state.movieList.concat(response.data.results);
+            return {
+              movieList
+            };
+          });
         });
-      });
+    }
   }
 
   render() {
-   const nameList = this.state.movieList.map(movie => {
+    const nameList = this.state.movieList.map(movie => {
       return movie.original_title;
     });
     return (
       <div className='App'>
+        {console.log('movie list', this.state.movieList)}
         <h1>Movie Listing</h1>
-        <SearchBar suggestions={nameList} movieList={this.state.movieList}/>
-        
+        <SearchBar suggestions={nameList} movieList={this.state.movieList} />
       </div>
     );
   }
